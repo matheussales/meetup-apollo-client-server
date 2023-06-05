07234dbe27b8f073aec14f7062366614b2494592
Matheus Martins de Sales <m.martins.sales@hotmail.com>
2023-06-05 13:45:53 -0300

    finaliza codigo

--- index.js
+++ index.js
@@ -2,69 +2,74 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `
-    type Produto {
+    type Product {
         id: ID!
-        nome: String
-        preco: Int
-        categoria: Categoria
+        name: String
+        price: Int
+        category: Category
     }
 
-    type Categoria {
+    type Category {
         id: ID!
-        nome: String
+        name: String
     }
 
     type Query {
-        produtos: [Produto]
+        products: [Product]
     }
 
-    input ProdutoInput {
-        nome: String
-        preco: Int
+    input ProductInput {
+        name: String
+        price: Int
     }
 
     type Mutation {
-        addProduto(produto: ProdutoInput): Produto
-        alteraProduto(produto: ProdutoInput): Produto
-        deleteProduto(id: ID!): Boolean
+        addProduct(product: ProductInput): Product
+        alteraProduct(product: ProductInput): Product
+        deleteProduct(id: ID!): Boolean
     }
 `;
 
 
-const produtos = [
+const products = [
     {
         id: 1,
-        nome: 'Samsung Galaxy A54',
-        preco: 1000,
-        categoria: {
+        name: 'Samsung Galaxy A54',
+        price: 1000,
+        category: {
             id: 1,
-            nome: 'Celular'
+            name: 'Celular'
         }
     },
     {
         id: 2,
-        nome: 'Samsung Galaxy S22',
-        preco: 2000,
-        categoria: {
+        name: 'Samsung Galaxy S22',
+        price: 2000,
+        category: {
             id: 1,
-            nome: 'Celular'
+            name: 'Celular'
         }
     },
     {
         id: 3,
-        nome: 'Iphone 13',
-        preco: 5000,
-        categoria: {
+        name: 'Iphone 13',
+        price: 5000,
+        category: {
             id: 1,
-            nome: 'Celular'
+            name: 'Celular'
         }
     },
 ];
 
 const resolvers = {
     Query: {
-        produtos: () => {
-            return produtos; 
+        products: () => {
+            return products; 
+        }
+    },
+    Mutation: {
+        addProduct: (_, { product }) => {
+            return { id: 4, ...product }; 
         }
     }
 };
